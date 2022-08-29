c9db88e513bdc3a048a5bd8511a00542da178e44
Revathi G <revathikrishna0323@gmail.com>
2022-08-29 18:36:36 +0530

    intial commit

--- src/main/java/com/jap/marksevaluation/OakBridgeSchool.java
+++ src/main/java/com/jap/marksevaluation/OakBridgeSchool.java
@@ -1,58 +1,7 @@
 package com.jap.marksevaluation;
+
+import java.util.Arrays;
+
 public class OakBridgeSchool {
 
-
-    // Convert the given string name to upper case
-    public String convertToUpperCase(String name){
-       return null;
-    }
-
-    // Find the shortest word in the list of surnames
-    public String shortestSurname(String[] surnames)
-    {
-        return " ";
-    }
-
-    // Check if any of the names in the array is repeated, if yes return true else false
-    public boolean checkIfNamesAreRepeated(String[] names){
-        boolean flag = false;
-
-        return flag;
-    }
-
-    // Concatenate the name and surname of the students
-    public String[] concatenateNameAndSurname(String[] names, String [] surnames){
-        String []  concatenatedNames = null;
-
-        return  concatenatedNames;
-    }
-
-    // Display the names
-    public void displayStudentNames(String[] namesArray){
-
-
-    }
-
-    public static void main(String[] args) {
-        String[] studentName = {"Michelle", "Kate", "Ann", "Tina", "Tom", "Sam", "Ria","Pam", "Kate", "Leo"};
-        String[] studentSurname = {"Smith", "Jhonson", "Williams", "Brown", "Jones", "Miller", "Davis", "Martin", "Clark", "King"};
-        OakBridgeSchool oakBridgeSchool = new OakBridgeSchool();
-        // Declare and Initialize values to the arrays
-        String[] studentNames = null;
-        String[] studentSurnames = null;
-        // Call the method to convert all the student names to upper case and display results
-        //create a variable with class name to cal method
-        OakBridgeSchool oakBridgeSchool1 = new OakBridgeSchool();
-        String[] upper = OakBridgeSchool.convertToUpperCase(studentNames);
-        // Call the method to sort names in alphabetical order and display results
-        String[]  sortedNames = OakBridgeSchool.
-
-        // Call the method to find the shortest surname and display results
-
-        // Call the method to check if two names in the array are the same and display results
-
-        // Call the method to concatenate name and surname with a space in between and display results
-
-
-    }
 }
