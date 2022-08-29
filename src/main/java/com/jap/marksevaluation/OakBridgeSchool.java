5ccf3e768556836c971153e57b03f3596f59f487
Revathi G <revathikrishna0323@gmail.com>
2022-08-29 18:37:38 +0530

    added main method, declared and intialised String arrays

--- src/main/java/com/jap/marksevaluation/OakBridgeSchool.java
+++ src/main/java/com/jap/marksevaluation/OakBridgeSchool.java
@@ -4,4 +4,10 @@ import java.util.Arrays;
 
 public class OakBridgeSchool {
 
+    public static void main(String[] args) {
+        OakBridgeSchool oakBridgeSchool = new OakBridgeSchool();
+        // Declare and Initialize values to the arrays
+        String[] studentNames = {"Michelle", "Kate", "Ann", "Tina", "Tom", "Sam", "Ria", "Pam", "Kate", "Leo"};
+        String[] studentSurnames = {"Smith", "Johnson", "Williams", "Brown", "Jones", "Miller", "Davis", "Martin", "Clark", "King"};
+
 }
